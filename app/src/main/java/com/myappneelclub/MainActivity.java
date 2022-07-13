93cd7d36175da7bb7b45f553adb7cc5f7515c6ff
Aarti Patel <royal.arati10@gmail.com>
2022-07-13 12:56:33 +0530

    Explicit Intent

--- app/src/main/java/com/myappneelclub/MainActivity.java
+++ app/src/main/java/com/myappneelclub/MainActivity.java
@@ -2,13 +2,36 @@ package com.myappneelclub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtEmail;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edt_email);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              String strEmail =   edtEmail.getText().toString();
+
+                Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                i.putExtra("KEY_EMAIL",strEmail);
+                startActivity(i);
+
+            }
+        });
+
+
     }
 }
